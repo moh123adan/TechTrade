be5272812a14fd0d985ec256899f65bd24254d34
moh123adan <maxamedadan169@gmail.com>
2024-10-27 16:53:59 +0300

    added: layout and slug to the category

---
+++ src/app/categories/_layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+
+export default function CategoryLayout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="slug"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+}
