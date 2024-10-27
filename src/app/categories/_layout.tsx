ee08aede9672d58e95968763ba00abda40b669c3
moh123adan <maxamedadan169@gmail.com>
2024-10-27 17:09:15 +0300

    added: navigation to category layout

--- src/app/categories/_layout.tsx
+++ src/app/categories/_layout.tsx
@@ -1,13 +1,20 @@
 import { Stack } from "expo-router";
+import { TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function CategoryLayout() {
   return (
     <Stack>
       <Stack.Screen
         name="slug"
-        options={{
-          headerShown: false,
-        }}
+        options={({ navigation }) => ({
+          headerShown: true,
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Ionicons name="arrow-back" size={24} color="black" />
+            </TouchableOpacity>
+          ),
+        })}
       />
     </Stack>
   );
