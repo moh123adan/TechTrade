81e8287fdf84f7df8f5375b91bef20428a809bd5
moh123adan <maxamedadan169@gmail.com>
2024-10-29 17:04:45 +0300

    added: flatlist

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,11 +1,29 @@
+import { PRODUCTS } from "@/assets/products";
 import { StyleSheet, View, Text } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 
 export default function index() {
   return (
     <View>
-      <Text>Home</Text>
+      <FlatList
+        data={PRODUCTS}
+        renderItem={({ item }) => <View></View>}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
+        ListHeaderComponent={<Text>Products</Text>}
+        contentContainerStyle={styles.flatListContent}
+        columnWrapperStyle={styles.flatListColumn}
+        style={{ paddingHorizontal: 10, paddingVertical: 5 }}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  flatListContent: {
+    paddingBottom: 20,
+  },
+  flatListColumn: {
+    justifyContent: "space-between",
+  },
+});
